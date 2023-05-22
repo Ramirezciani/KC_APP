61679428c880f805c2139eaccabac382ad1f7d97
Ramirezciani <pau.ramirez@duocuc.cl>
2023-05-22 11:51:57 -0400

    antes de cagarla commit , validacion de usuario

--- kidsConnect/src/app/pages/login/login.page.ts
+++ kidsConnect/src/app/pages/login/login.page.ts
@@ -14,33 +14,24 @@ export class LoginPage {
 
   constructor(private router: Router, private dbService: DbService) { }
 
-  login() {
-    this.validateCredentials();
-  }
+  validarUsuario() {
+    const datosValidos = this.dbService.validarCredenciales(this.rut, this.password);
 
-  validateCredentials() {
-    this.dbService.validateCredentials(this.rut, this.password)
-      .then((valid: boolean) => {
-        if (valid) {
-          // Datos válidos, redirigir a la página principal
-          this.router.navigate(['/principal']);
-        } else {
-          // Datos inválidos, mostrar mensaje de error o tomar acciones adicionales
-          console.log('Datos inválidos');
-        }
-      })
-      .catch((error: any) => {
-        // Error en la validación de las credenciales, mostrar mensaje de error o tomar acciones adicionales
-        console.log('Error en la validación de las credenciales:', error);
-      });
-  }
-
-  cancelar() {
-    // Acción del botón Cancelar
-    console.log('Cancelar');
+    if (datosValidos) {
+      // Los datos son válidos, redirigir a la página principal
+      this.router.navigate(['/principal']);
+    } else {
+      // Los datos son inválidos
+      console.log('Datos inválidos');
+    }
   }
 
   Ir_recuperar() {
     this.router.navigate(['/recuperar-pass']);
   }
+
+  ir_login() {
+    this.router.navigate(['/principal']);
+  }
+
 }
