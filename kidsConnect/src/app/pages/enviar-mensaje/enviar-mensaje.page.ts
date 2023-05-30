d456a935242186f799d35b12ab21d137d36ac9d1
Ramirezciani <pau.ramirez@duocuc.cl>
2023-05-29 22:49:56 -0400

    no se envia mensaje

--- kidsConnect/src/app/pages/enviar-mensaje/enviar-mensaje.page.ts
+++ kidsConnect/src/app/pages/enviar-mensaje/enviar-mensaje.page.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 import { MensajeService } from 'src/app/services/mensajes.service';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
-// ...
-
 @Component({
   selector: 'app-enviar-mensaje',
   templateUrl: 'enviar-mensaje.page.html',
@@ -13,11 +11,15 @@ import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 export class EnviarMensajePage {
   nombre: string = '';
   docentes: any[] = [];
-  docenteSeleccionado : any  = [];
+  docenteSeleccionado: any = null;
   mensaje: string = '';
   nombreEmisor: string = '';
+  tipoMensaje: string = '';
+  rutEmisor: any ;
 
-  constructor(private mensajeService: MensajeService, private http:HttpClient) {}
+  constructor(private mensajeService: MensajeService, private http: HttpClient) {
+    this.rutEmisor = localStorage.getItem('rutUsuario');
+  }
 
   async buscarDocentes() {
     try {
@@ -35,43 +37,66 @@ export class EnviarMensajePage {
     }
   }
 
-  
-
   enviarMensaje() {
     if (this.docenteSeleccionado) {
       console.log('Docente seleccionado:', this.docenteSeleccionado.rut_participante);
       console.log('Nombre del docente seleccionado:', this.docenteSeleccionado.nombre_completo);
       console.log('Mensaje:', this.mensaje);
-      console.log('Rut del emisor:', localStorage.getItem('rut_emisor'));
+      console.log('Rut del emisor:', this.rutEmisor);
       console.log('Nombre del emisor:', this.nombreEmisor);
-  
+
       // Obtén los datos necesarios para la solicitud POST
       const rutReceptor = this.docenteSeleccionado.rut_participante;
       const nomReceptor = this.docenteSeleccionado.nombre_completo;
       const contMensaje = this.mensaje;
-      const rutEmisor = localStorage.getItem('rut_emisor');
+      const rutEmisor = this.rutEmisor;
       const nomEmisor = this.nombreEmisor;
-  
+
+      // Map the selected type to the API type value
+      let tipoMensajeApiValue = '';
+      switch (this.tipoMensaje) {
+        case 'Justificativo':
+          tipoMensajeApiValue = 'J';
+          break;
+        case 'Comunicacion':
+          tipoMensajeApiValue = 'C';
+          break;
+        case 'Comprobante':
+          tipoMensajeApiValue = 'P';
+          break;
+      }
+
       // Crea el objeto de datos para la solicitud POST
       const data = {
-        rutParticipante: rutReceptor,
-        nombreParticipante: nomReceptor,
-        contMensaje: contMensaje
+        rut_emisor: rutEmisor,
+        nom_emisor: nomEmisor,
+        rut_receptor: rutReceptor,
+        nom_receptor: nomReceptor,
+        cont_mensaje: contMensaje,
+        img_mensaje: '',
+        tipo_mensaje: tipoMensajeApiValue
       };
-  
+
       // Realiza la solicitud POST a través de la API
       this.http.post<any>('http://tmp.enred.cl/kc/rest/mensajes-movil.php', data).subscribe(
-        (response: HttpResponse<any>) => {
-          console.log('Mensaje enviado:', response.body.mensaje);
-          // Resto de la lógica después de enviar el mensaje...
-        },
-        (error: HttpErrorResponse) => {
-          console.log('Error al enviar el mensaje:', error.error);
-        }
-      );
-  
+  (response: any) => {
+    if (response && response.body && response.body.mensaje) {
+      console.log('Mensaje enviado:', response.body.mensaje);
+      // Resto de la lógica después de enviar el mensaje...
     } else {
-      console.log('No se ha seleccionado ningún docente');
+      console.log('Respuesta desconocida');
+    }
+  },
+  (error: HttpErrorResponse) => {
+    if (error && error.error) {
+      console.log('Error al enviar el mensaje:', error.error);
+    } else {
+      console.log('Error desconocido');
     }
   }
+);
+    }
+  }
+  
+  
 }  
