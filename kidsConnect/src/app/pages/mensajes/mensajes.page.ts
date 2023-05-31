d3e72fedf0165776d8b65ffdb1a7fa2c980bb290
Ramirezciani <pau.ramirez@duocuc.cl>
2023-05-30 20:06:57 -0400

    logica lista de mostrar los mensajes

--- kidsConnect/src/app/pages/mensajes/mensajes.page.ts
+++ kidsConnect/src/app/pages/mensajes/mensajes.page.ts
@@ -1,19 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
-import { ApiService } from 'src/app/services/api.service';
+import { MensajeService } from 'src/app/services/mensajes.service';
 
-interface Mensaje {
-  cod_mensaje: string; 
-  fch_crea: string; 
-  img_msj: string;
-  cont_msj: string; 
-  tip_msj_cod_tip_m: number; 
-  usuario_rut_us: number;
-  usuario_tipo_us_cod_tip: number; 
-  nom_tip_m: string;
-
-}
 
 @Component({
   selector: 'app-mensajes',
@@ -21,26 +10,29 @@ interface Mensaje {
   styleUrls: ['./mensajes.page.scss'],
 })
 export class MensajesPage implements OnInit {
-  mensajes: Mensaje[] = [];
-  filtroMensaje: string = '';
+  
+  mensajes : any[] = [];
 
-  constructor(private router: Router, private apiService: ApiService,
+  constructor(private router: Router, private mensajeService: MensajeService,
               private menuCtrl:MenuController) {}
 
-  ngOnInit() {
-    this.cargarMensajes();
-  }
-
-  cargarMensajes() {
-    this.apiService.obtenerMensajesAll().subscribe(
-      (data: Mensaje[]) => {
-        this.mensajes = data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+  
+              ngOnInit() {
+                const rutUsuario = localStorage.getItem('rutUsuario'); // Ajusta la clave utilizada para almacenar el rut en el LocalStorage
+              
+                if (rutUsuario !== null) {
+                  this.mensajeService.getMensajesByRut(rutUsuario).subscribe(
+                    (response: Object) => {
+                      this.mensajes = response as any[];
+                    },
+                    (error) => {
+                      console.log(error);
+                    }
+                  );
+                } else {
+                  console.log('El valor de rutUsuario es null');
+                }
+              }
 
   bandeja_entrada() {
     this.router.navigate(['/mensaje-recibido']);
