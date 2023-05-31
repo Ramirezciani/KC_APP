ae79c214a1f67cd7f305d65f29d1a9cdd1a1b891
Ramirezciani <pau.ramirez@duocuc.cl>
2023-05-31 17:08:36 -0400

    version 31 -05

--- kidsConnect/src/app/pages/enviar-mensaje/enviar-mensaje.page.ts
+++ kidsConnect/src/app/pages/enviar-mensaje/enviar-mensaje.page.ts
@@ -1,16 +1,15 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MensajeService } from 'src/app/services/mensajes.service';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { MenuController, ToastController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-enviar-mensaje',
   templateUrl: 'enviar-mensaje.page.html',
   styleUrls: ['enviar-mensaje.page.scss']
 })
-
-//Variables que mensaje necesita para post y para obtener resultados en la busqueda
 export class EnviarMensajePage {
   nombre: string = '';
   docentes: any[] = [];
@@ -21,17 +20,10 @@ export class EnviarMensajePage {
   rutEmisor: number;
   
 
-  constructor(private mensajeService: MensajeService, 
-              private http: HttpClient,
-              private toastController: ToastController,
-              private menuCtrl:MenuController) {
-
-//Valor del rut del usuario obtenido desde el login page
-
+  constructor(private mensajeService: MensajeService, private http: HttpClient, private menuCtrl:MenuController) {
     this.rutEmisor = parseInt(localStorage.getItem('rutUsuario') || '0');
   }
 
-  //Funcion que busca docentes, al apretar el boton trae todos los docentes de la lista curso y/o se puede buscar por nombre 
   async buscarDocentes() {
     try {
       const response: any = await this.mensajeService.obtenerDocentes(this.nombre).toPromise();
@@ -39,10 +31,8 @@ export class EnviarMensajePage {
 
       if (response && response.success) {
         this.docentes = response.data;
-        this.presentToast('bottom', 'Docentes encontrados con exito');
         console.log('Docentes encontrados:', this.docentes);
       } else {
-        this.presentToast('bottom', 'No existen resultado para tu busqueda');
         console.log('No se encontraron docentes con el nombre especificado');
       }
     } catch (error) {
@@ -50,11 +40,8 @@ export class EnviarMensajePage {
     }
   }
 
-  //Funcion para enviar el mensaje, console logs solo a modo de prueba para ver que parametros se estan guardando, las variables const 
-  //son los datos a enviar.
   async enviarMensaje() {
-    if (!this.docenteSeleccionado) {
-      this.presentToast('bottom', 'Por favor selecciona un docente');
+    if (this.docenteSeleccionado) {
       console.log('Docente seleccionado:', this.docenteSeleccionado.rut_participante);
       console.log('Nombre del docente seleccionado:', this.docenteSeleccionado.nombre_completo);
       console.log('Mensaje:', this.mensaje);
@@ -68,6 +55,19 @@ export class EnviarMensajePage {
       const rutEmisor = this.rutEmisor;
       const nomEmisor = this.nombreEmisor;
 
+      // Map the selected type to the API type value
+      // let tipoMensajeApiValue = '';
+      // switch (this.tipoMensaje) {
+      //   case 'Justificativo':
+      //     tipoMensajeApiValue = 'J';
+      //     break;
+      //   case 'Comunicacion':
+      //     tipoMensajeApiValue = 'C';
+      //     break;
+      //   case 'Comprobante':
+      //     tipoMensajeApiValue = 'P';
+      //     break;
+      // }
               let tipoMensajeApiValue = '';
         switch (this.tipoMensaje) {
           case 'Justificativo':
@@ -80,7 +80,8 @@ export class EnviarMensajePage {
             tipoMensajeApiValue = 'Comprobante';
             break;
           default:
-
+            // Si ninguna de las opciones coincide, puedes manejarlo como desees.
+            // En este ejemplo, se asigna un valor por defecto.
             tipoMensajeApiValue = '';
             break;
         }
@@ -96,39 +97,27 @@ export class EnviarMensajePage {
         tipo_mensaje: tipoMensajeApiValue
       };
 
-      //Envio de solicitud post a la URL definida donde se encuentra el endopoint. Captura de errores mediante toastController 
 
       this.http.post('http://tmp.enred.cl/kc/rest/buzon.php', data, { responseType: 'text' }).subscribe(
         (response: any) => {
           console.log('Mensaje enviado:', response);
-          this.presentToast('bottom', 'Mensaje Enviado');
           // Resto de la lógica después de enviar el mensaje...
         },
         (error: HttpErrorResponse) => {
           if (error && error.error) {
-            this.presentToast('bottom', 'Error al enviar mensaje a' );
             console.log('Error al enviar el mensaje:', error.error);
           } else {
             console.log('Error desconocido');
           }
         }
       );
+      }
     }
-  }
 
-  //Funcion que implementa el toast en la pagina
-  async presentToast(position: 'top' | 'middle' | 'bottom', message: string) {
-    const toast = await this.toastController.create({
-      message: message,
-      duration: 1500,
-      position: position,
-    });
-
-    await toast.present();
-  }
 
   //Funcion para abrir el menu lateral
   onClick() {
     this.menuCtrl.toggle();
   }
 }
+
