352f0e2322e0446cc9e9f34e6df9f1156e621f46
Ramirezciani <pau.ramirez@duocuc.cl>
2023-06-13 15:20:40 -0400

    intento fallido de calendario con mensajes

--- kidsConnect/src/app/pages/eventos/eventos.page.ts
+++ kidsConnect/src/app/pages/eventos/eventos.page.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { MenuController, ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.page.html',
@@ -8,15 +9,51 @@ import { MenuController } from '@ionic/angular';
 })
 export class EventosPage implements OnInit {
 
-  constructor(private router:Router,
-              private menuCtrl: MenuController) { }
+  fechaSeleccionada: string = '';
+  mensajesFiltrados: any[] = [];
 
-  ngOnInit() {
+  constructor(
+    private menuCtrl: MenuController,
+    private toastController: ToastController,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {}
+
+  // Función para abrir el menú lateral
+  onClick() {
+    this.menuCtrl.toggle();
+  }
+
+  // Función para mostrar los mensajes de tipo anuncio y/o evento
+  async filtrarMensajesPorFecha() {
+    if (this.fechaSeleccionada) {
+      try {
+        const fechaSeleccionada = new Date(this.fechaSeleccionada).toISOString().substring(0, 10);
+        const url = 'http://tmp.enred.cl/html/kc/rest/get_anuncios.php'; // Reemplaza por la URL de tu API
+  
+        const response = await this.http.get<any[]>(url).toPromise();
+        if (typeof response !== 'undefined') {
+          this.mensajesFiltrados = response.filter(mensaje => mensaje.fecha.substring(0, 10) === fechaSeleccionada);
+        } else {
+          this.mensajesFiltrados = [];
+        }
+      } catch (error) {
+        console.error('Error al obtener los mensajes:', error);
+        this.mostrarMensajeError('Error al obtener los mensajes');
+      }
+    } else {
+      this.mensajesFiltrados = [];
+    }
   }
   
-  onClick(){
-    this.menuCtrl.toggle();
 
+  async mostrarMensajeError(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 3000,
+      color: 'danger'
+    });
+    toast.present();
   }
-
 }
