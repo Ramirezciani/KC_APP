f50b1ce15cc2e27e9517a1ebe28cf1e9146bc41e
Ramirezciani <pau.ramirez@duocuc.cl>
2023-06-14 22:56:01 -0400

    filtro de mensajes por mes

--- kidsConnect/src/app/pages/eventos/eventos.page.ts
+++ kidsConnect/src/app/pages/eventos/eventos.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MenuController } from '@ionic/angular';
+import { MenuController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-eventos',
@@ -9,8 +9,14 @@ import { MenuController } from '@ionic/angular';
 })
 export class EventosPage implements OnInit {
   mensajes: any[] = [];
+  mensajesFiltrados: any[] = []; // Array para almacenar los mensajes filtrados
+  mesSeleccionado: string = ''; // Variable para almacenar el mes seleccionado
 
-  constructor(private http: HttpClient, private menuCtrl: MenuController) {}
+  constructor(
+    private http: HttpClient,
+    private menuCtrl: MenuController,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
     this.obtenerMensajes();
@@ -23,6 +29,8 @@ export class EventosPage implements OnInit {
         if (response && response.length > 0) {
           console.log(response); // Verificar la respuesta en la consola
           this.mensajes = response;
+          this.mensajesFiltrados = response; // Inicialmente, los mensajes filtrados serán iguales a todos los mensajes
+          this.mostrarToast();
         } else {
           console.log('La respuesta está vacía');
         }
@@ -31,6 +39,30 @@ export class EventosPage implements OnInit {
         console.error('Error al obtener los mensajes:', error);
       }
     );
+  }
+
+  filtrarMensajes() {
+    if (this.mesSeleccionado) {
+      // Aplicar filtro solo si se ha seleccionado un mes
+      this.mensajesFiltrados = this.mensajes.filter((mensaje) => {
+        const fechaMensaje = new Date(mensaje.fecha_envio);
+        const mesMensaje = fechaMensaje.getMonth() + 1; // El mes en JavaScript se indexa desde 0, por lo que se suma 1
+        return mesMensaje.toString() === this.mesSeleccionado;
+      });
+      this.mostrarToast();
+    } else {
+      this.mensajesFiltrados = this.mensajes; // Si no se ha seleccionado un mes, mostrar todos los mensajes
+    }
+  }
+
+  async mostrarToast() {
+    const cantidadMensajes = this.mensajesFiltrados.length;
+    const toast = await this.toastController.create({
+      message: `Hay ${cantidadMensajes} mensajes en el mes seleccionado`,
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 
   onClick() {
